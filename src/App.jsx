a0b853ad2ffd7506b26cbaec64124636e1a08741
ninazmul <nazmulsaw@gmail.com>
2024-06-13 12:00:19 +0600

    Footer design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Home from "./Page/Home";
+import Footers from "./components/Footers";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <Home />
+      <Footers />
     </>
   )
 }
---
+++ src/components/Footers.jsx
@@ -0,0 +1,64 @@
+import { Footer } from "flowbite-react";
+import {
+  BsDribbble,
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsTwitter,
+} from "react-icons/bs";
+import { FaOpencart } from "react-icons/fa6";
+
+export default function Footers() {
+  return (
+    <Footer container>
+      <div className="w-full px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div className="flex items-center gap-2 cursor-pointer pb-10 md:p-auto">
+            <FaOpencart className="text-4xl font-bold" />
+            <h1 className="self-center whitespace-nowrap text-3xl font-bold font-serif dark:text-white">
+              <span className="text-orange-500">E</span>-Cart
+            </h1>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="about" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#"> E-commerce Cart</Footer.Link>
+                <Footer.Link href="#">Shop Now</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Github</Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <p className="text-gray-500 text-sm">
+            {" "}
+            &copy; {new Date().getFullYear()} E-commerce Cart. All rights
+            reserved.
+          </p>
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
